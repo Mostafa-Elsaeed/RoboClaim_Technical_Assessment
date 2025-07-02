5062f7dff1212489affbc28ee3b050fb592975fd
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2025-07-03 00:59:54 +0300

    Work Done: - add rabbit mq micro-service to the app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
-import { NestFactory } from "@nestjs/core";
-import { Logger } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 
 // Local Imports
-import { AppModule } from "./app.module";
-import { catchErrors } from "./global-helpers/catch-errors";
+import { AppModule } from './app.module';
+import { catchErrors } from './global-helpers/catch-errors';
+import { Transport } from '@nestjs/microservices';
 
 async function startApp() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@192.168.1.201:5672'],
+      queue: 'file_processing_queue',
+      queueOptions: { durable: false },
+    },
+  });
+
+  await app.startAllMicroservices();
+  Logger.log('RabbitMQ Microservice is running');
   await app.listen(port);
   Logger.log(`Application is running on: http://localhost:${port}`);
 }
@@ -16,7 +29,7 @@ async function bootstrap() {
   const [app, error] = await catchErrors(() => startApp());
 
   if (error) {
-    Logger.error("Error starting application:", error);
+    Logger.error('Error starting application:', error);
     process.exit(1);
   }
 }
