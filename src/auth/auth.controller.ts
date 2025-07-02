448d47d2c2c67ffb5ba35fd97fd67b36dd41d521
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2025-07-02 16:21:36 +0300

    Work Done: - unify the auth controller shape

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,22 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { SignUpDto } from "./dto/signup.dto";
-import { SignInDto } from "./dto/signin.dto";
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
+import { SignInDto } from './dto/signin.dto';
 // import { join } from "path";
 
-@Controller("auth")
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
-  @Post("login")
+  @Post('login')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
-  @Post("signup")
   @HttpCode(HttpStatus.OK)
+  @Post('signup')
   async signUp(@Body() signUp: SignUpDto) {
-    // console.log(signUp);
     return await this.authService.signUp(signUp);
   }
 }
