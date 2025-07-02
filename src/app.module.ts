d3c37e4a248af74b363fe05cf93b02ce23e3af36
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2025-07-02 12:40:13 +0300

    Work Done: - implement env validation for db and jwt and app - add user entity to test typeorm - create the configuration of typeorm and connect it to neon postgresql - add dotenv library to make the env stored easly and can be shared secure

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+// LOCAL IMPORTS
+import config from "./configs/env/env.configs";
+
+import { envValidationSchema } from "./configs/env/validation.schema";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppDataSource } from "./database/database.config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      cache: true,
+      // envFilePath: [".env"],
+      validationSchema: envValidationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+})
+export class AppModule {}
